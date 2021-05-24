d139eff9a5e9032e6d9528518e5d0c92fb668641
Kiranadil <kiranadil10@gmail.com>
2021-05-24 23:07:41 +0500

    applied console.log to function

--- practice projects/2/app.js
+++ practice projects/2/app.js
@@ -5,5 +5,13 @@ function chooseMedium(){
     alert('Hello this is level Medium');
 }
 function chooseHard(){
-    alert('Hello this is level Hard');
+    // alert('Hello this is level Hard');
+    console.log('1');
+    console.log('2');
+    console.log('3');
+    console.log('4');
+    console.log('5');
+    console.log('6');
+    console.log('7');
+    console.log('8');
 }
