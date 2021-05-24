f6855f064357fb15c7eb8c1759df7f0e96c9e2f3
Kiranadil <kiranadil10@gmail.com>
2021-05-24 22:56:47 +0500

    added border radius tto button

---
+++ practice projects/2/app.js
@@ -0,0 +1,9 @@
+function chooseEasy(){
+    alert('Hello this is level Easy');
+}
+function chooseMedium(){
+    alert('Hello this is level Medium');
+}
+function chooseHard(){
+    alert('Hello this is level Hard');
+}
