d15debabc883b9ba0d2fcaf1c41e84c15d74c88d
Kiranadil <kiranadil10@gmail.com>
2021-05-31 13:52:29 +0500

    used for loop in array

---
+++ practice projects/7 array/array.js
@@ -0,0 +1,12 @@
+let students = [0,29,78,30,6,3,2,1,5,67, 45,67,75]; // array
+// roll number--> index
+for ( let i = 0; i< students.length; i++){
+    console.log("student#"+ (i+1) +  students[i]);
+}
+//for loop in an array
+let myArray = ["one","two", "three","four"];
+
+    for(let i=0; i<myArray.length ; i++)
+    {
+      console.log(myArray[i]);
+    }
