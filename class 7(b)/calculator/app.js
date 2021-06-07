d1a6943492540ec50fb75c17f81cf2a8e1093066
Kiranadil <kiranadil10@gmail.com>
2021-06-07 15:43:30 +0500

    changed the width of <div> element

--- class 7(b)/calculator/app.js
+++ class 7(b)/calculator/app.js
@@ -1,9 +1,32 @@
 function addNumbers(){
-    let num1=parseFloat(document.querySelector("#firstNumb").value);
-    let num2=parseFloat(document.querySelector("#secondNumber").value);
-    result = num1 + num2;
+    let num1=parseInt(document.querySelector("#firstNumber").value);
+    let num2=parseInt(document.querySelector("#secondNumber").value);
+    let result=parseInt(num1)+parseInt(num2);
+    const list=document.querySelector("#result > span");
+    list.append(result);
     console.log(result);
 }
 function subtractNumbers(){
-
+    let num1=parseInt(document.querySelector("#firstNumber").value);
+    let num2=parseInt(document.querySelector("#secondNumber").value);
+    let result=parseInt(num1)-parseInt(num2);
+    const list=document.querySelector("#result > span");
+    list.append(result);
+    console.log(result);
+}
+function multiplyNumbers(){
+    let num1=parseInt(document.querySelector("#firstNumber").value);
+    let num2=parseInt(document.querySelector("#secondNumber").value);
+    let result=parseInt(num1) * parseInt(num2);
+    const list=document.querySelector("#result > span");
+    list.append(result);
+    console.log(result);
+}
+function divideNumbers(){
+    let num1=parseInt(document.querySelector("#firstNumber").value);
+    let num2=parseInt(document.querySelector("#secondNumber").value);
+    let result=parseInt(num1) / parseInt(num2);
+    const list=document.querySelector("#result > span");
+    list.append(result);
+    console.log(result);
 }
